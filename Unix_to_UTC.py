6cc8a62b312076793f74ca397c4aaf4a12d8518b
Jauaries <49643029+Grenosaurus@users.noreply.github.com>
2020-04-01 16:17:55 +0300

    Update Unix_to_UTC.py

--- Unix_to_UTC.py
+++ Unix_to_UTC.py
@@ -1,5 +1,5 @@
 """
- Program converts Unix timestamp into date (Made: Jauaries Loyala -- 15.01.2020)
+ Program converts Unix timestamp into date and time form (Made: Jauaries Loyala -- 15.01.2020).
 """
 
 # Python packets
